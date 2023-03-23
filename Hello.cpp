27b5712948bb73cd803ed7db55b92bbf03e0a40b
SHAIK FARIDHUDEEN <100666867+Shaik0709@users.noreply.github.com>
2023-03-23 10:41:17 +0530

    Hello World program in cpp

---
+++ Hello.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// int main ()
+// {
+//     std::cout <<"hello world";
+//     return 0;
+// }
+
+/*
+    without using std in all the line so use (using namespace std;)
+    for new line \n or <<endl -(use the using namespace std;)
+
+*/
+// #include <iostream>
+// using namespace std;
+// int main ()
+// {
+//     cout <<"hello world";
+//     return 0;
+// }
+// #include<iostream>
+// #include<conio.h>
+// using namespace std;
+// int main ()
+// {
+//     cout<<"welcome to c++ programming";
+//     getch();
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the value of a :";
+    cin >> a;
+    cout << "value of a  is " << a;
+    return 0;
+}
