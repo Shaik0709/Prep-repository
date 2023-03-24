6890c28de6fb1a6637501a4be00d313bbdd05c92
SHAIK FARIDHUDEEN <100666867+Shaik0709@users.noreply.github.com>
2023-03-24 15:57:50 +0530

    Comments and Escape sequence in C program...

---
+++ Comments_Escape_sequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+
+    // Single Line Comment ...
+    /*
+       Multiline Comment ...
+    */
+    /* escape sequence = character combination consisting of a backslash \
+     Followed by a letter or combination of digits.
+    They specify actions within a line or string of text.
+    \n newline
+    \t = tab
+ */
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\n \"He love Burger \"\n");
+    return 0;
+}
