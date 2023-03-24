72fa71181468527a6c15b882e48025319757e929
SHAIK FARIDHUDEEN <100666867+Shaik0709@users.noreply.github.com>
2023-03-24 17:37:42 +0530

    Format specifiers in C program

---
+++ Format-specifiers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    //Format specifiers % = defines and formats a type of data to be displayed
+    // %c - character
+    // %d - Integer
+    // %s - String (Array of characters)
+    // %f - Float
+    // %lf - Double 
+    // %.1 - Decimal Precision
+    // %1 - Minimum feild width 
+    // %- = Left align
+    return 0;
+}
