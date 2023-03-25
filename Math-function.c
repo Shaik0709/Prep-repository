1607959396ce1d24c78eea15c3ee710aee9b8637
Shaik <shaiknijas7@gmail.com>
2023-03-25 11:44:21 +0530

    Math.h fun is created in prep-repo

---
+++ Math-function.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+
+    double A = sqrt(9);
+    double B = pow(2,4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    double E = floor(3.99);
+    double F = fabs(-100); // Absolute neg to positive value
+    double G = log(3);
+    double H = sin(30);
+    double I = cos(45);
+    double J = tan(90);
+    printf("\n%lf",A);
+    printf("\n%lf",B);
+    printf("\n%d",C);
+    printf("\n%d",D);
+    printf("\n%lf",E);
+    printf("\n%lf",F);
+    printf("\n%lf",G);
+    printf("\n%lf",H);
+    printf("\n%lf",I);
+    printf("\n%lf",J);
+    return 0;
+}
