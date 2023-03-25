b9ad2d1a818867fa3d7f4ebd7f95b57125388c7f
Shaik <shaiknijas7@gmail.com>
2023-03-25 11:25:40 +0530

    string.h is added in file User-input.c

--- user-input.c
+++ user-input.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<string.h>
 int main()
 {
     char name[25];
@@ -8,6 +9,7 @@ int main()
     fgets(name, 25, stdin);
     printf("\nHow old are you ? :");
     scanf("%d",&age);
+    name[strlen(name)-1]='\0'; // To avoid printing output in new line
 
     printf("\nHello %s How are you...", name);
     printf("\nyour are  %d years old ...", age);
