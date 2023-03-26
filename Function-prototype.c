de82495e610e8b71d187406f7a5d2b337e486fc9
Shaik <shaiknijas7@gmail.com>
2023-03-26 13:52:49 +0530

    Function Argument in C

---
+++ Function-prototype.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+// Function Prototype
+/*
+    what is it ?
+    Function Declaration , without a body , before main ()
+    ensures that calls to a function are made with correct arguments
+*/
+// void Hello(char name[] , int age );
+int main ()
+{
+    char name[]="Akber Ali";
+    int age = 5;
+
+ Hello (name,age);
+    return 0;
+}
+/*
+    Many c compilers do not check for parameter matching
+    Missing arguments will result in unexpected behavior
+    A function prototype causes the compiler to flag an error if arguments are missing
+*/
+void Hello(char name[] , int age)
+{
+    printf("\n Hello  %s  , Happy birthday to you ",name);
+    printf("\n Your are %d years old now ... ",age);
+    
+}
