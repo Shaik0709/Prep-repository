2c62812a094e4e0aaf18fbbc2bcd68c2dc140dd2
Shaik <shaiknijas7@gmail.com>
2023-03-26 12:01:41 +0530

    Function.c in C

---
+++ functions.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void birthday()
+{
+    printf("\n Happy Birthday to You \n Enjoy your day ... \n"); //Gloabl function...
+};
+int main ()
+{
+    birthday();
+    birthday();
+
+    return 0;
+}
