0ba5c86044b56ee1f13f639789c3022cbbf54245
Shaik <shaiknijas7@gmail.com>
2023-03-27 13:44:37 +0530

    Array.c

---
+++ Array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    // Array - a data structure that can store many values of the same data type.
+    double Prices[] = {12.3, 14.5, 17.9, 20.3, 24.5};
+    printf("$%.2lf", Prices[4]);
+    return 0;
+}
