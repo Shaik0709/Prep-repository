5743acf681b139897a6a0711f6511fdff5305d8f
Shaik <shaiknijas7@gmail.com>
2023-03-27 13:48:53 +0530

    Array with loop to print prices

--- Array.c
+++ Array.c
@@ -3,6 +3,11 @@ int main()
 {
     // Array - a data structure that can store many values of the same data type.
     double Prices[] = {12.3, 14.5, 17.9, 20.3, 24.5};
-    printf("$%.2lf", Prices[4]);
+    for (int  i = 0; i < 5; i++)
+    {
+        printf("$%.2lf\n",Prices[i]);
+    }
+    
+    //printf("$%.2lf", Prices[4]);
     return 0;
 }
