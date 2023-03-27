ebf236fc10dff9f55fac99b94b554c05e32a57f4
Shaik <shaiknijas7@gmail.com>
2023-03-27 11:27:27 +0530

    For loop in C

---
+++ for-loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    // for loop - repeats a section of code a limited amount of times
+    for (int i = 1 ; i <=10 ;i++)
+    {
+        printf("%d\t",i);
+    }
+    return 0;
+
+}
